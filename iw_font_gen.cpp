cd16a9b1ecc95634f5c2fa0ebf226d330ddf2647
GEEKiDoS <geek_ds@foxmail.com>
2020-12-25 09:00:59 +0800

    cp1252

--- iw_font_gen.cpp
+++ iw_font_gen.cpp
@@ -1,5 +1,6 @@
 ﻿#include <vector>
 #include <iostream>
+#include <string>
 
 #include "imgui.h"
 #include "imgui_impl_dx9.h"
@@ -22,6 +23,8 @@
 static LPDIRECT3D9              g_pD3D = NULL;
 static LPDIRECT3DDEVICE9        g_pd3dDevice = NULL;
 static D3DPRESENT_PARAMETERS    g_d3dpp = {};
+
+using namespace std::string_literals;
 
 // Forward declarations of helper functions
 bool CreateDeviceD3D(HWND hWnd);
@@ -94,6 +97,27 @@ ImWchar* GetTextRangeCommonChinese(const  ImWchar* baseRange)
 	return output_range.Data;
 }
 
+ImWchar cp1252[] = { 0x0000, 0x0001, 0x0002, 0x0003, 0x0004, 0x0005, 0x0006, 0x0007, 0x0008, 0x0009, 0x000A, 0x000B, 0x000C, 0x000D, 0x000E, 0x000F, 0x0010, 0x0011, 0x0012, 0x0013, 0x0014, 0x0015, 0x0016, 0x0017, 0x0018, 0x0019, 0x001A, 0x001B, 0x001C, 0x001D, 0x001E, 0x001F, 0x0020, 0x0021, 0x0022, 0x0023, 0x0024, 0x0025, 0x0026, 0x0027, 0x0028, 0x0029, 0x002A, 0x002B, 0x002C, 0x002D, 0x002E, 0x002F, 0x0030, 0x0031, 0x0032, 0x0033, 0x0034, 0x0035, 0x0036, 0x0037, 0x0038, 0x0039, 0x003A, 0x003B, 0x003C, 0x003D, 0x003E, 0x003F, 0x0040, 0x0041, 0x0042, 0x0043, 0x0044, 0x0045, 0x0046, 0x0047, 0x0048, 0x0049, 0x004A, 0x004B, 0x004C, 0x004D, 0x004E, 0x004F, 0x0050, 0x0051, 0x0052, 0x0053, 0x0054, 0x0055, 0x0056, 0x0057, 0x0058, 0x0059, 0x005A, 0x005B, 0x005C, 0x005D, 0x005E, 0x005F, 0x0060, 0x0061, 0x0062, 0x0063, 0x0064, 0x0065, 0x0066, 0x0067, 0x0068, 0x0069, 0x006A, 0x006B, 0x006C, 0x006D, 0x006E, 0x006F, 0x0070, 0x0071, 0x0072, 0x0073, 0x0074, 0x0075, 0x0076, 0x0077, 0x0078, 0x0079, 0x007A, 0x007B, 0x007C, 0x007D, 0x007E, 0x007F, 0x20AC, 0x20, 0x201A, 0x0192, 0x201E, 0x2026, 0x2020, 0x2021, 0x02C6, 0x2030, 0x0160, 0x2039, 0x0152, 0x0000, 0x017D, 0x0000, 0x0000, 0x2018, 0x2019, 0x201C, 0x201D, 0x2022, 0x2013, 0x2014, 0x02DC, 0x2122, 0x0161, 0x203A, 0x0153, 0x0000, 0x017E, 0x0178, 0x00A0, 0x00A1, 0x00A2, 0x00A3, 0x00A4, 0x00A5, 0x00A6, 0x00A7, 0x00A8, 0x00A9, 0x00AA, 0x00AB, 0x00AC, 0x00AD, 0x00AE, 0x00AF, 0x00B0, 0x00B1, 0x00B2, 0x00B3, 0x00B4, 0x00B5, 0x00B6, 0x00B7, 0x00B8, 0x00B9, 0x00BA, 0x00BB, 0x00BC, 0x00BD, 0x00BE, 0x00BF, 0x00C0, 0x00C1, 0x00C2, 0x00C3, 0x00C4, 0x00C5, 0x00C6, 0x00C7, 0x00C8, 0x00C9, 0x00CA, 0x00CB, 0x00CC, 0x00CD, 0x00CE, 0x00CF, 0x00D0, 0x00D1, 0x00D2, 0x00D3, 0x00D4, 0x00D5, 0x00D6, 0x00D7, 0x00D8, 0x00D9, 0x00DA, 0x00DB, 0x00DC, 0x00DD, 0x00DE, 0x00DF, 0x00E0, 0x00E1, 0x00E2, 0x00E3, 0x00E4, 0x00E5, 0x00E6, 0x00E7, 0x00E8, 0x00E9, 0x00EA, 0x00EB, 0x00EC, 0x00ED, 0x00EE, 0x00EF, 0x00F0, 0x00F1, 0x00F2, 0x00F3, 0x00F4, 0x00F5, 0x00F6, 0x00F7, 0x00F8, 0x00F9, 0x00FA, 0x00FB, 0x00FC, 0x00FD, 0x00FE, 0x00FF };
+
+ImWchar* GetTextRangeEAscii()
+{
+	ImFontGlyphRangesBuilder builder;
+
+	for (int i = 32; i < 256; i++)
+	{
+		if (cp1252[i] == 0)
+			continue;
+
+		builder.AddChar(cp1252[i]);
+	}
+
+	static ImVector<ImWchar> output_range;
+
+	builder.BuildRanges(&output_range);
+
+	return output_range.Data;
+}
+
 
 int main()
 {
@@ -136,7 +160,7 @@ int main()
 
 	ImFontConfig cfg;
 
-	ImFont* font = io.Fonts->AddFontFromFileTTF(font_name.data(), pixelSize, &cfg, GetTextRangeCommonChinese(io.Fonts->GetGlyphRangesChineseSimplifiedCommon()));
+	ImFont* font = io.Fonts->AddFontFromFileTTF(font_name.data(), pixelSize, &cfg, GetTextRangeEAscii());
 
 	//
    // Our state
@@ -194,9 +218,9 @@ int main()
 			ImGui::Text("English Text Preview");
 			ImGui::Text("the quick brown fox jumps over a lazy dog.");
 			ImGui::Text("THE QUICK BROWN FOX JUMPS OVER A LAZY DOG.");
-			ImGui::Text(u8"中文文本预览：");
-			ImGui::Text(u8"一去二三里烟村四五家亭台六七座八九十支花");
-			ImGui::Text(u8"中国智造，慧及全球");
+			// ImGui::Text(u8"中文文本预览：");
+			// ImGui::Text(u8"一去二三里烟村四五家亭台六七座八九十支花");
+			// ImGui::Text(u8"中国智造，慧及全球");
 
 			ImGui::InputTextMultiline("Input Test", buffer, 10000);
 
@@ -208,40 +232,63 @@ int main()
 				std::cout << "Where: ";
 				std::cin >> where;
 
-				auto textureFile = where + ".png";
+				auto textureFile = where + ".dds";
 
-				D3DXSaveTextureToFileA(textureFile.data(), _D3DXIMAGE_FILEFORMAT::D3DXIFF_PNG, (LPDIRECT3DBASETEXTURE9)io.Fonts->TexID, 0);
+				D3DXSaveTextureToFileA(textureFile.data(), _D3DXIMAGE_FILEFORMAT::D3DXIFF_DDS, (LPDIRECT3DBASETEXTURE9)io.Fonts->TexID, 0);
 
 				FILE* f;
 
-				auto err = fopen_s(&f, where.data(), "w");
+				auto err = fopen_s(&f, (where + ".json").data(), "w");
 
 				fprintf(f, "{");
-				fprintf(f, "\"fontName\":\"%s\",", "fonts/name");
-				fprintf(f, "\"material\":\"%s\",", "fonts/name");
-				fprintf(f, "\"glowMaterial\":\"%s\",", "fonts/name_glow");
+				fprintf(f, "\"fontName\":\"%s\",", ("fonts/"s + where).data());
+				fprintf(f, "\"material\":\"%s\",", ("fonts/"s + where).data());
+				fprintf(f, "\"glowMaterial\":\"%s\",", ("fonts/"s + where + "_glow"s).data());
 				fprintf(f, "\"pixelHeight\":%d,", (int)io.Fonts->Fonts[0]->FontSize);
 				fprintf(f, "\"glyphs\": [");
 
 				std::vector<int> list;
 
-				for (int i = 0; i < io.Fonts->Fonts[0]->Glyphs.Size; i++)
-					list.push_back(i);
-
-				std::sort(list.begin(), list.end(), [io](int a, int b)
+				int j = 0;
+				for (int i = 32; i < 256; i++)
 				{
-					return (io.Fonts->Fonts[0]->Glyphs[a].Codepoint < io.Fonts->Fonts[0]->Glyphs[b].Codepoint);
-				});
+					int pc = cp1252[i];
 
-				for (int i = 0; i < io.Fonts->Fonts[0]->Glyphs.Size; i++)
+					if (cp1252[i] == 0)
+						pc = '#';
+
+					int pos = 0;
+
+					for (int j = 0; j < io.Fonts->Fonts[0]->Glyphs.Size; j++)
+					{
+						auto& g = io.Fonts->Fonts[0]->Glyphs[j];
+
+						if (g.Codepoint == pc)
+						{
+							pos = j;
+							break;
+						}
+					}
+
+					list.push_back(pos);
+				}
+
+				//for (int i = 0; i < io.Fonts->Fonts[0]->Glyphs.Size; i++)
+				//	list.push_back(i);
+				//
+				//std::sort(list.begin(), list.end(), [io](int a, int b)
+				//	{
+				//		return (WidetoGBK(io.Fonts->Fonts[0]->Glyphs[a].Codepoint) < WidetoGBK(io.Fonts->Fonts[0]->Glyphs[b].Codepoint));
+				//	});
+
+				int count = 0;
+
+				for (int i = 32; i < 256; i++)
 				{
-					ImFontGlyph& glyph = io.Fonts->Fonts[0]->Glyphs[list[i]];
-
-					if (glyph.Codepoint < 32)
-						continue;
+					ImFontGlyph& glyph = io.Fonts->Fonts[0]->Glyphs[list[i - 32]];
 
 					fprintf(f, "{");
-					fprintf(f, "\"letter\":%d,", glyph.Codepoint);
+					fprintf(f, "\"letter\":%d,", i);
 					fprintf(f, "\"dx\":%d,", (int)glyph.AdvanceX);
 					fprintf(f, "\"pixelHeight\":%d,", (int)round(glyph.Y1 - glyph.Y0));
 					fprintf(f, "\"pixelWidth\":%d,", (int)round(glyph.X1 - glyph.X0));
@@ -253,13 +300,15 @@ int main()
 					fprintf(f, "\"t1\":%f", glyph.V1);
 
 
-					if (i == io.Fonts->Fonts[0]->Glyphs.Size - 1)
+					if (i == 255)
 						fprintf(f, "}");
 					else
 						fprintf(f, "},");
+
+					count++;
 				}
 				fprintf(f, "],");
-				fprintf(f, "\"glyphCount\":%d", list.size());
+				fprintf(f, "\"glyphCount\":%d", count);
 				fprintf(f, "}");
 
 				fclose(f);
